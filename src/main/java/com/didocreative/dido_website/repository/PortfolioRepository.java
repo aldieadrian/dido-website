bebda31946dc132e347b556c70df21188abd721f
Aldie Adrian Rachmadi <aldieadrian@Aldies-Macbook-Pro.local>
2025-11-05 22:25:21 +0700

    building repository

--- src/main/java/com/didocreative/dido_website/repository/PortfolioRepository.java
+++ src/main/java/com/didocreative/dido_website/repository/PortfolioRepository.java
@@ -1,4 +1,22 @@
 package com.didocreative.dido_website.repository;
 
-public interface PortfolioRepository {
+import com.didocreative.dido_website.model.Portfolio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
+
+    // JpaRepository<Portfolio, Long> gives us:
+    // - save(Portfolio entity)
+    // - findById(Long portfolioId)
+    // - findAll()
+    // - deleteById(Long portfolioId)
+    // - ...and many more, all for free!
+
+    // Get Portfolios by projectTypeId
+    List<Portfolio> findByProjectId(Long projectTypeId);
+
 }
