57228632ee52737397ddc59a093c9a653209513d
Aldie Adrian Rachmadi <aldieadrian@Aldies-Macbook-Pro.local>
2025-11-10 23:01:27 +0700

    creating service

---
+++ src/main/java/com/didocreative/dido_website/service/PortfolioService.java
@@ -0,0 +1,51 @@
+package com.didocreative.dido_website.service;
+
+import com.didocreative.dido_website.model.Portfolio;
+import com.didocreative.dido_website.model.PortfolioCopywritingSection;
+import com.didocreative.dido_website.model.PortfolioImageSection;
+import com.didocreative.dido_website.repository.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+//    @Service: This is the most important annotation.
+//    It tells Spring "This is a service bean. Create an object
+//    of this class and keep it in memory so others can @Autowire it."
+@Service
+public class PortfolioService {
+
+    @Autowired
+    private PortfolioRepository portfolioRepository;
+
+    @Autowired
+    private PortfolioImageSectionRepository imageSectionRepository;
+
+    @Autowired
+    private PortfolioImageRepository imageRepository;
+
+    @Autowired
+    private PortfolioCopywritingSectionRepository copywritingSectionRepository;
+
+    @Autowired
+    private PortfolioCopywritingRepository copywritingRepository;
+
+    public List<Portfolio> getAllPortfolios(){
+        return portfolioRepository.findAll();
+    }
+
+    public Portfolio getPortfolioDetail(Long portfolioId){
+        Portfolio portfolio = portfolioRepository.findById(portfolioId).orElse(null);
+
+        if (portfolio != null){
+
+            List<PortfolioImageSection> imageSections = imageSectionRepository.findByPortfolioId(portfolioId);
+
+            List<PortfolioCopywritingSection> copywritingSections = copywritingSectionRepository.findByPortfolioId(portfolioId);
+        }
+
+        return portfolio;
+    }
+
+
+
+}
